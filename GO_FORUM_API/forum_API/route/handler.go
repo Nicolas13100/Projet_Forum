9363f61a1f97f31b0d1b04cc9acff25805213f2a
Nicolas <nicolas.moyon@ynov.com>
2024-06-25 10:51:03 +0200

    update login and register

--- GO_FORUM_API/forum_API/route/handler.go
+++ GO_FORUM_API/forum_API/route/handler.go
@@ -12,51 +12,51 @@ func RUN() {
 	r := mux.NewRouter()
 
 	// Setting up HTTP routes for different endpoints
-	r.HandleFunc("/getHome", API.Authenticate(API.GetAllTopic)).Methods("GET")
-	r.HandleFunc("/getPost/{id}", API.Authenticate(API.GetTopic)).Methods("GET")
-	r.HandleFunc("/getUser/{id}", API.Authenticate(API.GetUser)).Methods("GET")
-	r.HandleFunc("/getAllTopicMessage/{id}", API.Authenticate(API.GetAllTopicMessage)).Methods("GET")
-	r.HandleFunc("/getAllMessageAnswer/{id}", API.Authenticate(API.GetAllMessageAnswer)).Methods("GET")
+	r.HandleFunc("/getHome", API.GetAllTopic).Methods("GET")
+	r.HandleFunc("/getPost/{id}", API.GetTopic).Methods("GET")
+	r.HandleFunc("/getUser/{id}", API.GetUser).Methods("GET")
+	r.HandleFunc("/getAllTopicMessage/{id}", API.GetAllTopicMessage).Methods("GET")
+	r.HandleFunc("/getAllMessageAnswer/{id}", API.GetAllMessageAnswer).Methods("GET")
 
 	// Admin
-	r.HandleFunc("/modifyTopic", API.Authenticate(API.ModifyTopicHandler)).Methods("POST")
-	r.HandleFunc("/banUser", API.Authenticate(API.BanUserHandler)).Methods("POST")
+	r.HandleFunc("/modifyTopic", API.ModifyTopicHandler).Methods("POST")
+	r.HandleFunc("/banUser", API.BanUserHandler).Methods("POST")
 
 	// Logging
 	r.HandleFunc("/register", API.RegisterHandler).Methods("POST")
 	r.HandleFunc("/login", API.LoginHandler).Methods("POST")
-	r.HandleFunc("/logout", API.Authenticate(API.LogoutHandler)).Methods("POST")
-	r.HandleFunc("/dashboard", API.Authenticate(API.DashboardHandler)).Methods("GET")
-	r.HandleFunc("/changeUserData", API.Authenticate(API.ChangeUserDataHandler)).Methods("POST")
+	r.HandleFunc("/logout", API.LogoutHandler).Methods("POST")
+	r.HandleFunc("/dashboard", API.DashboardHandler).Methods("GET")
+	r.HandleFunc("/changeUserData", API.ChangeUserDataHandler).Methods("POST")
 
 	// Create Topic
-	r.HandleFunc("/createTopic", API.Authenticate(API.CreateTopicHandler)).Methods("POST")
-	r.HandleFunc("/deleteTopic", API.Authenticate(API.DeleteTopicHandler)).Methods("DELETE")
-	r.HandleFunc("/deleteComment", API.Authenticate(API.DeleteCommentHandler)).Methods("DELETE")
+	r.HandleFunc("/createTopic", API.CreateTopicHandler).Methods("POST")
+	r.HandleFunc("/deleteTopic", API.DeleteTopicHandler).Methods("DELETE")
+	r.HandleFunc("/deleteComment", API.DeleteCommentHandler).Methods("DELETE")
 
 	// Like Topic
-	r.HandleFunc("/likeTopic", API.Authenticate(API.LikeTopicHandler)).Methods("POST")
-	r.HandleFunc("/dislikeTopic", API.Authenticate(API.DislikeTopicHandler)).Methods("POST")
+	r.HandleFunc("/likeTopic", API.LikeTopicHandler).Methods("POST")
+	r.HandleFunc("/dislikeTopic", API.DislikeTopicHandler).Methods("POST")
 
 	// Favorite Topic
-	r.HandleFunc("/favoriteTopic", API.Authenticate(API.FavoriteTopicHandler)).Methods("POST")
+	r.HandleFunc("/favoriteTopic", API.FavoriteTopicHandler).Methods("POST")
 
 	// Comment Topic
-	r.HandleFunc("/commentTopic", API.Authenticate(API.CommentHandler)).Methods("POST") // work for both comment a topic and comment a comment
-	r.HandleFunc("/updateComment", API.Authenticate(API.UpdateCommentHandler)).Methods("POST")
+	r.HandleFunc("/commentTopic", API.CommentHandler).Methods("POST") // work for both comment a topic and comment a comment
+	r.HandleFunc("/updateComment", API.UpdateCommentHandler).Methods("POST")
 
 	// Like Comment
-	r.HandleFunc("/likeComment", API.Authenticate(API.LikeCommentHandler)).Methods("POST")
-	r.HandleFunc("/dislikeComment", API.Authenticate(API.DislikeCommentHandler)).Methods("POST")
+	r.HandleFunc("/likeComment", API.LikeCommentHandler).Methods("POST")
+	r.HandleFunc("/dislikeComment", API.DislikeCommentHandler).Methods("POST")
 
 	// UserFriends
-	r.HandleFunc("/addFriend", API.Authenticate(API.AddFriendHandler)).Methods("POST")
-	r.HandleFunc("/acceptFriend", API.Authenticate(API.AcceptFriendHandler)).Methods("POST")
-	r.HandleFunc("/declineFriend", API.Authenticate(API.DeclineFriendHandler)).Methods("POST")
-	r.HandleFunc("/deleteFriend", API.Authenticate(API.DeleteFriendHandler)).Methods("POST")
+	r.HandleFunc("/addFriend", API.AddFriendHandler).Methods("POST")
+	r.HandleFunc("/acceptFriend", API.AcceptFriendHandler).Methods("POST")
+	r.HandleFunc("/declineFriend", API.DeclineFriendHandler).Methods("POST")
+	r.HandleFunc("/deleteFriend", API.DeleteFriendHandler).Methods("POST")
 
 	// Search
-	r.HandleFunc("/search", API.Authenticate(API.SearchHandler)).Methods("GET")
+	r.HandleFunc("/search", API.SearchHandler).Methods("GET")
 
 	// Serve static files from the "forum_API/static" directory
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("forum_API/static"))))
