9d4dc0ea42fbf419b5e3739761da42a62f538f42
Nicolas <nicolas.moyon@ynov.com>
2024-06-25 11:17:46 +0200

    update login and register

--- ServWeb_JS/backend/serv.js
+++ ServWeb_JS/backend/serv.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, 'data.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
